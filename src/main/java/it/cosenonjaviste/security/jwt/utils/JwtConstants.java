f7e1cce7f835971e0916a9083c558fdb908ee993
Joost Aafjes <joostaafjes@gmail.com>
2016-12-13 16:14:14 +0100

    rename header

--- src/main/java/it/cosenonjaviste/security/jwt/utils/JwtConstants.java
+++ src/main/java/it/cosenonjaviste/security/jwt/utils/JwtConstants.java
@@ -2,7 +2,7 @@ package it.cosenonjaviste.security.jwt.utils;
 
 /**
  * Helper class for centralizing constants
- * 
+ *
  * @author acomo
  *
  */
@@ -11,16 +11,16 @@ public class JwtConstants {
 	/**
 	 * Authentication header
 	 */
-	public static final String AUTH_HEADER = "X-Auth";
+	public static final String AUTH_HEADER = "Authorization";
 	
 	/**
 	 * User Id claim key
 	 */
 	public static final String USER_ID = "userId";
-	
+
 	/**
 	 * Roles claim key
 	 */
-	public static final String ROLES = "roles";	
-	
+	public static final String ROLES = "roles";
+
 }
